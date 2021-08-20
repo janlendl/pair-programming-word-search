ae3a823b6b32c1d26ee7920732a25ab62c4a4828
Lendl Peralta <janlendl@gmail.com>
2021-08-20 03:10:38 +0000

    completion of pair programming activitiy with @chazdean

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  let resultHorizontal = false;
+  let resultVertical = false;
 
-module.exports = wordSearch
+  resultHorizontal = search(letters, word);
+  resultVertical = search(convertVertical(letters), word);
+  
+  if (resultVertical || resultHorizontal) {
+    return true;
+  }
+  return false;
+};
+
+// Convert array matrix to grab the vertical letters
+const convertVertical = (letters) => {
+  const verticalArray = [];
+  
+  for (let x = 0; x < letters.length - 1; x++) {
+    const tempArray = [];
+    for (let y = 0; y < letters[x].length + 1; y++) {
+      tempArray.push(letters[y][x]);
+    }
+    verticalArray.push(tempArray);
+  }
+  return verticalArray;
+};
+
+const search = (letters, word) => {
+  const result = letters.map(ls => ls.join(''));
+  for (l of result) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
